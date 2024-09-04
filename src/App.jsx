c4fa8eeca08f18997d9229e6329e953f869e5b54
Mohammad <mmdwfrh@gmail.com>
2024-09-04 14:47:09 +0330

    Onclick & fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
+import { useState } from "react";
 
 
 
 
 function App() {
+
+  const [excuse, setExcuse] = useState('')
+
+  const handleClick = (e) => {
+    fetch(`https://excuser-three.vercel.app/v1/excuse/${e.target.value}`)
+    .then(res => res.json())
+    .then(data => setExcuse(data[0].excuse))
+  }
+
   return (
     <div className="container text-center">
       <h1 className="my-5">Generate an excuse</h1>
 
       <div className="btn-group mb-5">
-        <button className="btn btn-primary text-light">Party</button>
-        <button className="btn btn-primary text-light">Family</button>
-        <button className="btn btn-primary text-light">Office</button>
+        <button className="btn btn-primary text-light" value="party" onClick={handleClick}>Party</button>
+        <button className="btn btn-primary text-light" value="family" onClick={handleClick}>Family</button>
+        <button className="btn btn-primary text-light" value="office" onClick={handleClick}>Office</button>
       </div>
 
       <div className="w-75 p-5 mx-auto border rounded">
-        <h2></h2>
+        <h2>{excuse}</h2>
       </div>
     </div>
   )
