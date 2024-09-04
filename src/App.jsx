fbc2a192a957fe7946b170869ceb08a4082a92cf
Mohammad <mmdwfrh@gmail.com>
2024-09-04 14:38:50 +0330

    JSX template designed using Bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+
+
+
+function App() {
+  return (
+    <div className="container text-center">
+      <h1 className="my-5">Generate an excuse</h1>
+
+      <div className="btn-group mb-5">
+        <button className="btn btn-primary text-light">Party</button>
+        <button className="btn btn-primary text-light">Family</button>
+        <button className="btn btn-primary text-light">Office</button>
+      </div>
+
+      <div className="w-75 p-5 mx-auto border rounded">
+        <h2></h2>
+      </div>
+    </div>
+  )
+}
+
+
+
+export default App;
